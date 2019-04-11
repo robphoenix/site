c2cba34c68c36452a3441ef99937c9703b4c21f2
Rob Phoenix <rob@robphoenix.com>
2019-04-11 20:08:15 +0100

    Conditionally show title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,63 +1,38 @@
-// @ts-nocheck
 import React from 'react'
-import { Global, css } from '@emotion/core'
+import { Global } from '@emotion/core'
 import { ThemeProvider } from 'emotion-theming'
 
 import theme from '../theme'
 import Navbar from '../components/Navbar'
 
 const Layout = ({ location, title, children }) => {
-  const page = location.pathname.replace(/\//g, '') || 'home'
+  const page = location.pathname.replace(/\//g, ``) || `home`
   const colour = theme.colors[page] || theme.colors.posts
 
-  // const rootPath = `${__PATH_PREFIX__}/`
+  const rootPath = `${__PATH_PREFIX__}/`
+  const onHomePage = location.pathname === rootPath
 
-  // if (location.pathname === rootPath) {
-  //   header = <Link to={`/`}>{title}</Link>
-  // } else {
-  //   header = <Link to={`/`}>{title}</Link>
-  // }
   return (
     <ThemeProvider theme={theme}>
       <Global
-        styles={css`
-          body {
-            background-color: ${colour};
-            transition: background-color 0.2s ease-out;
-          }
-        `}
+        styles={{
+          body: {
+            backgroundColor: `${colour}`,
+          },
+        }}
       />
       <div
-        css={css`
-          height: 100%;
-          width: 100%;
-        `}
+        css={{
+          height: `100%`,
+          width: `100%`,
+        }}
       >
+        {onHomePage ? `` : <h1>{title}</h1>}
         <Navbar />
         {children}
       </div>
     </ThemeProvider>
   )
 }
-
-// class Layout extends React.Component {
-//   render() {
-//     const { location, title, children, bg } = this.props
-
-//     return (
-//       <div
-//         css={css`
-//           background-color: ${bg};
-//           height: 100%;
-//           width: 100%;
-//         `}
-//       >
-//         <Navbar />
-//         {header}
-//         {children}
-//       </div>
-//     )
-//   }
-// }
 
 export default Layout
