e7f27d4953c932eb87c2ce28b6af42fc3cb7d9e7
Rob Phoenix <rob@robphoenix.com>
2019-04-11 09:17:04 +0100

    Add theme file

---
+++ src/theme.js
@@ -0,0 +1,40 @@
+const theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fontSizes: [12, 14, 16, 18, 20, 24, 36, 48, 80, 96],
+  fontWeights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  lineHeights: {
+    solid: 1,
+    title: 1.25,
+    copy: 1.5,
+  },
+  letterSpacings: {
+    normal: 'normal',
+    tracked: '0.1em',
+    tight: '-0.05em',
+    mega: '0.25em',
+  },
+  fonts: {
+    header: 'Space Mono',
+    body:
+      'Karla,  "-apple-system", BlinkMacSystemFont, "avenir next", avenir, "helvetica neue", helvetica, ubuntu, roboto, noto, "segoe ui", arial, sans- serif',
+  },
+  borders: [
+    0,
+    '1px solid',
+    '2px solid',
+    '4px solid',
+    '8px solid',
+    '16px solid',
+    '32px solid',
+  ],
+  width: [16, 32, 64, 128, 256],
+  heights: [16, 32, 64, 128, 256],
+  maxWidths: [16, 32, 64, 128, 256, 512, 768, 1024, 1536],
+  colors: {
+    home: 'hotpink',
+    blog: 'goldenrod',
+    posts: 'lightblue',
+  },
+}
+
+export default theme
