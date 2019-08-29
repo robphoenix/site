5f51a0feec0d43095bc48cf5f8ea118210b25378
Rob Phoenix <rob@robphoenix.com>
2019-08-29 13:01:31 +0100

    Limit mobile spacing

--- src/components/Spacing.js
+++ src/components/Spacing.js
@@ -6,24 +6,32 @@ const Spacing = () => {
   const { space } = useTheme()
   space.shift()
 
+  const mobileLimit = 128
+
   return (
     <ul sx={{ listStyle: 'none', display: 'flex', flexDirection: 'column' }}>
-      {space.map((s, i) => (
+      {space.map(s => (
         <li
+          key={s}
           sx={{
-            display: 'flex',
+            display: s > mobileLimit ? ['none', 'flex'] : 'flex',
           }}
         >
           <div
             sx={{
-              height: 1,
+              height: 2,
               width: `${s}px`,
               backgroundColor: 'primary',
               marginBottom: 2,
-              marginRight: 2,
+              marginRight: 1,
+              fontFamily: 'body',
+              fontSize: [1, 2, 2],
+              display: 'flex',
+              alignItems: 'center',
             }}
-          />
-          <span sx={{ fontFamily: 'body', fontSize: [1, 2, 2] }}>{s}px</span>
+          >
+            {s}px
+          </div>
         </li>
       ))}
     </ul>
--- src/components/TypeScale.js
+++ src/components/TypeScale.js
@@ -14,6 +14,7 @@ const TypeScale = () => {
     >
       {fontSizes.map((fontSize, i) => (
         <div
+          key={fontSize}
           sx={{
             fontSize: i,
             fontFamily: 'body',
